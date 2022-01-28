91d928b40a1f5283df9f850ef187e665ac77d1c0
Влад Коростыленко <vlad.korostylenko@gmail.com>
2022-01-28 22:36:00 +0200

    Add file

--- main.py
+++ main.py
@@ -14,21 +14,49 @@
 #     print ("Значение должно быть больше 0. Пожалуйста, попробуйте снова.")
 
 # 2. Пользователь вводит трехзначное число. Найдите сумму его цифр. (используйте %)
-
+#
 # three_digit_number = int(input("Введите целое, трехзначное число: "))
 #
-# left_from_100 = int(three_digit_number//100)
-# left_from_10 = int(three_digit_number%100//10)
-# left_from_1 = int(three_digit_number%100%10)
+# if 100 <= three_digit_number <= 999:
+#     left_from_100 = int(three_digit_number//100)
+#     left_from_10 = int(three_digit_number%100//10)
+#     left_from_1 = int(three_digit_number%100%10)
 #
-# sum_of_numbers = int(left_from_100 + left_from_10 + left_from_1)
+#     sum_of_numbers = int(left_from_100 + left_from_10 + left_from_1)
 #
-# print (sum_of_numbers)
+#     print (f"Сумма трех цифр из числа {three_digit_number} равна: {sum_of_numbers}")
+#
+# elif three_digit_number <= 99 or three_digit_number >= 1000:
+#     print ("Пожалуйста, используйте только трехзначные числа")
 
 # 3. Найти максимальное число из трех. Числа вводится с клавиатуры
-# Определить високосный год или нет.Число вводится с клавиатуры
-# Определить четное или нечетное число. Число вводится с клавиатуры
-# Задания с повышенной сложностью на 100 баллов
+#
+first_number = int(input("Введите первое число: "))
+
+second_number = int(input("Введите второе число: "))
+
+third_number = int(input("Введите третье число: "))
+
+if first_number >= second_number and first_number >= third_number:
+    print (f"Наибольшее число: {first_number}")
+
+elif second_number >= first_number and second_number >= third_number:
+    print(f"Наибольшее число: {second_number}")
+
+else third_number >= first_number and third_number >= second_number:
+    print(f"Наибольшее число: {third_number}")
+
+
+# 4. Определить високосный год или нет.Число вводится с клавиатуры
+
+input_year = int("Введите год: ")
+
+
+
+# 5. Определить четное или нечетное число. Число вводится с клавиатуры
+
+
+# 6. Задания с повышенной сложностью на 100 баллов
 #
 # Найти корни квадратного уравнения и вывести их на экран, если они есть. Если корней нет, то вывести сообщение об этом. Конкретное квадратное уравнение определяется коэффициентами a, b, c, которые вводит пользователь.
 # Дана следующая функция y=f(x):
